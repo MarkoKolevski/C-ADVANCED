f7a227d17499a36581b11a1573b616ee70f75de8
MarkoKolevski <kolevskimarko686@gmail.com>
2025-05-07 18:29:47 +0200

    Add files via upload
    Prva domashna

---
+++ HomeWork1/HomeWork1/HomeWork1/ProductDetails.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork1
+{
+    public class ProductDetails
+    {
+         public int Id { get; set; }
+
+         public string Title { get; set; }
+
+         public double Price { get; set; }
+
+         public ProductDetails (int id, string title, double price)
+    {
+         Id = id;
+         Title = title;
+         Price = price;
+    }
+  }
+}
